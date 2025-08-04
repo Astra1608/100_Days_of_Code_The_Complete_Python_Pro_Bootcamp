a9e27b226b2063bad7c3b3103868f45ce6970c72
Neha Pandey <np081616@gmail.com>
2025-08-04 21:39:26 +0530

    Commit

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,7 @@
 # Functions with input
-
-def greet_with_name(name):
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"What is it like in {location}")
+greet_with(name= "Jack Bauer",location= "California")
 
 
-greet_with_name("Jack Bauer")
