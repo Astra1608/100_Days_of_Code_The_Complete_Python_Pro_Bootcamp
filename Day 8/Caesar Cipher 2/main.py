7bf47d9120c33d0e9f79323bdfc0bfa9f2d6fea1
Neha Pandey <np081616@gmail.com>
2025-08-05 14:59:26 +0530

    Commit

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -4,23 +4,42 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
-# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
-#  by the shift amount and print the decrypted text.
-# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
-#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
-
 def encrypt(original_text, shift_amount):
-    cipher_text = ""
+    encrypted_text = " "
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        index_no_for_encryption = (alphabet.index(letter) + shift_amount) % len(alphabet)#13
+        encrypted_text +=alphabet[index_no_for_encryption]
+    print(f"Here's the encoded result: {encrypted_text} ")
+
+def decrypt(original_text, shift_amount):
+    decrypted_text = " "
+    for letter in original_text:
+        index_no_for_encryption = (alphabet.index(letter) - shift_amount) % len(alphabet)  # 13
+        decrypted_text += alphabet[index_no_for_encryption]
+    print(f"Here's the decoded result: {decrypted_text} ")
+
+#combined encryption and decryption
+def caesar(user_direction,user_text,user_shift):
+    output_text = " "
+    for letter in user_text:
+        if user_direction=="decode":
+            user_shift *= -1
+
+        index_no_for_encryption = (alphabet.index(letter) + user_shift) % len(alphabet)  # 13
+        output_text += alphabet[index_no_for_encryption]
+    print(f"Here's the {user_direction}d result: {output_text} ")
 
 
-encrypt(original_text=text, shift_amount=shift)
+caesar(direction,text,shift)
+
+
+
+
+
+
+
+
+
 
 
 
